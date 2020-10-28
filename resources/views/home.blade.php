4fa0077dbbae988740361dc0e1a8d5f965b1da21
AVITA India <avitaind@gmail.com>
2020-10-28 20:06:17 +0530

    form background chagnes

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,38 +4,39 @@
 
 
 
- <div class="main-div" style="background-image: url('/images/background.jpg');">
+ <div class="main-div" style="background-image: url('/images/background.jpg'); padding:10px 50px;">
     <div class="container-fluid" >
         <div class="row justify-content-center">
             <div class="col-md-6" style="margin-top: 10%; text-align: center">
-               <img src="/images/giftsonair_logo.png" class="logo">
 
-                <h3 class="alert-heading">Follow these 2 simple steps to redeem: </h3>
+                <h3 class="alert-heading" style="font-weight:600; color:#fff;">Follow these 2 simple steps to redeem: </h3>
                 <hr>
                 <table>
                 <tr>
                   <td>
-                      <h4 class="steps-heading">1.  Fill up your purchase details along with a copy of the invoice and contact information.</h4>
+                      <h4 class="steps-heading"  style="color:#fff;">1.  Fill up your purchase details along with a copy of the invoice and contact information.</h4>
                   </td>
 
                 </tr>
                       <td>
-                          <h4 class="steps-heading">2.  Get ready to be blown away by the confirmation email of the prize you have won! </h4>
+                          <h4 class="steps-heading"  style="color:#fff;">2.  Get ready to be blown away by the confirmation email of the prize you have won! </h4>
 
                       </td>
                   </tr>
             <tr>
             <td>
-                <h4 class="bold-heading"><strong> Don't forget to follow and subscribe us on our social  media handles listed below.</strong></h4>
+                <h4 class="bold-heading"  style="color:#fff;"><strong> Don't forget to follow and subscribe us on our social  media handles listed below.</strong></h4>
              </td>
         </tr>
+       
 
                 </table>
 
                 <a href="https://www.facebook.com/AvitaIndia/" class="fa fa-facebook" aria-hidden="true"></a>
                 <a href="https://twitter.com/AvitaIndia" class="fa fa-twitter" aria-hidden="true"></a>
                 <a href="https://www.instagram.com/avitaind/" class="fa fa-instagram" aria-hidden="true"></a>
-
+                <br/>
+                <img src="/images/laptops.png" style="width:1000px; margin-top:10%;" class="logo"> 
 
             </div>
 
@@ -45,7 +46,7 @@
     flex-direction: column;
     min-width: 0;
     word-wrap: break-word;
-    background-color: #fff;
+    background-color: rgb(255, 255, 255, 0.1);
     background-clip: border-box;
     border: 1px solid rgba(0,0,0,.125);
     border-radius: 25px">
@@ -58,6 +59,7 @@
                         <div class="container">
                             <div class="row">
                                <div class="col-12">
+                             
                                     @if ($errors->any())
                                         <div class="alert alert-danger alert-dismissible" role="alert">
                                             <button type="button" class="close" data-dismiss="alert" aria-label="Close">
@@ -155,12 +157,12 @@
     </div>
 
 
-     <h2 class="desclimer"><strong>Disclaimer</strong></h3>
+     <h2 class="desclimer" style="color:#fff;"><strong>Disclaimer</strong></h3>
 <hr>
-     <p class="product-statement-list" style="text-align:center; margin:2%;">
+     <p class="product-statement-list" style="text-align:center; margin:2%; color:#fff;">
          Winners are selected through an auto-generated process by our systems. No giveaways are affiliated in any way with the dealerships or associated with any organizations related to the products mentioned in the giveaway. Winners will be notified through email.
          The rules of the contest are at the behest of the organizer and not following them will result in immediate disqualification. Winners are entitled to their prizes and will receive the said items in 15 days, unless they are out of stock, in that case the prizes
-         will be disbursed upon restocking. All products shown are for illustration purpose only. Actual product may vary. Offer valid from 1<sup>st</sup> Nov 2020 till 30<sup>th</sup> November 2020. Terms and Conditions apply.
+         will be disbursed upon restocking. All products shown are for illustration purpose only. Actual product may vary. Offer valid from 28<sup>th</sup> Oct 2020 till 15<sup>th</sup> November 2020. Terms and Conditions apply.
      </p>
  </div>
 @endsection
