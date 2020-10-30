4c4ccd62ad259587668b4734f251949e81d4de98
AVITA India <avitaind@gmail.com>
2020-10-30 12:58:10 +0530

    file size validation v4

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -36,8 +36,9 @@ class Handler extends ExceptionHandler
         //
     }
 
-    public function render($request, Exception $exception)
+   /* public function render($request, Exception $exception)
     {
         return parent::render($request, $exception);
     }
+    */
 }
